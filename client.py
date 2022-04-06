257b018fa35c36e0c561f1d4bf11110d84f5b4ef
cbravos5 <cnbravos5@gmail.com>
2022-04-06 14:43:38 -0300

    feat: add Server and Client classes

---
+++ client.py
@@ -0,0 +1,40 @@
+import socket, ssl
+from common import sendMsg, recvMsg
+
+class Client:
+
+    def __init__(self):
+        self.context = self.createContext()
+        self.connection = self.createConnection()
+
+    def createContext(self):
+        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+        context.verify_mode = ssl.CERT_REQUIRED
+        context.load_verify_locations("./serverCRT/server.crt")
+        context.load_cert_chain(certfile="./clientCRT/client.crt", keyfile="./clientCRT/client.key")
+        return context
+
+    def createConnection(self):
+        s = socket.socket()# socket.AF_INET, socket.SOCK_STREAM default
+        connection = self.context.wrap_socket(s, server_side=False)
+        return connection
+    
+    def startConnection(self, port, handler):
+        try:
+            self.connection.connect(('127.0.0.1', port))
+            handler(self.connection)
+            self.connection.close()
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    client = Client()
+
+    def dataHandler(connection):
+        sendMsg(connection, "Hello From Client!!!")
+        data = recvMsg(connection)
+        print(data)
+
+    client.startConnection(3000,dataHandler)
+
---
+++ server.py
@@ -0,0 +1,49 @@
+import socket, ssl
+from common import sendMsg, recvMsg
+
+class Server:
+
+    def __init__(self, port):
+        self.context = self.createContext()
+        self.socket = self.createSocket(port)
+        self.connection = None
+
+    def createContext(self):
+        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+        context.verify_mode = ssl.CERT_REQUIRED
+        context.load_verify_locations("./clientCRT/client.crt")
+        context.load_cert_chain(certfile="./serverCRT/server.crt", keyfile="./serverCRT/server.key")
+        context.keylog_filename = "keylog.log" # DEBUG PURPOSES
+        return context
+    
+    def createSocket(self, port):
+        bindsocket = socket.socket()
+        bindsocket.bind(('127.0.0.1', port))
+        bindsocket.listen(5)
+        return bindsocket
+
+    def startListening(self, dataHandler):
+        while True:
+            try:
+                newsocket, fromaddr = self.socket.accept()
+                self.connection = self.context.wrap_socket(newsocket, server_side=True)
+                try:
+                    dataHandler(self.connection) # uses connection.recv() and connection.send()
+                finally:
+                    self.connection.shutdown(socket.SHUT_RDWR)
+                    self.connection.close()
+            except Exception as e:
+                print(e)
+
+if __name__ == "__main__":
+    server = Server(3000)
+
+    def dataHandler(connection):
+        data = recvMsg(connection)
+        while data:
+            print(data)
+            sendMsg(connection, "Hello From Server")
+            data = recvMsg(connection)
+            
+
+    server.startListening(dataHandler)
